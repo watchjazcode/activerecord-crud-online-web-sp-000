b2fdf6b85f00c598d2e3f6a8c3421df51f8d23ae
Jazmin M <jazminmerckel@gmail.com>
2019-12-10 04:20:18 +0000

    Automatically backed up by Learn

--- db/migrate/20191210041411_create_movies.rb
+++ db/migrate/20191210041411_create_movies.rb
@@ -5,7 +5,7 @@ class CreateMovies < ActiveRecord::Migration[5.2]
       t.integer :release_date
       t.string :director
       t.string :lead
-      t.boolean :in_theatres
+      t.boolean :in_theaters
     end
   end
 end
